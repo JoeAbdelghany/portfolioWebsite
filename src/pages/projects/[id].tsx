3e4bd1be37e1086b57d7bd0a10a4ec318d5bb2de
Joe Abdelghany <=>
2023-08-06 17:25:40 +0200

    remove ssg from project page

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,33 +1,21 @@
 import { Component } from 'react';
 import axios, { AxiosResponse } from 'axios';
 
-interface Props {
-    projects: any,
-}
+interface Props { }
 
 class Project extends Component<Props> { 
     render() {
-        const { projects } = this.props;
+        // const { ______ } = this.props;
         return(
             <>
                 <section>
                     <div className="container grid grid-cols-12 gap-x-16">
-                        
+
                     </div>
                 </section>
             </>
         )
     }
 }
-
-export const getStaticProps = async () => {
-    const res: AxiosResponse = await axios.get(`${process.env.API_KEY}/api/projects`);
-    const projects: any = res.data;
-    return {
-        props: {
-            projects,
-        }
-    }
-};
 
 export default Project;
